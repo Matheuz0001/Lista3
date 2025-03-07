4a32742be62cf8f89ab3007303954604d660901b
garoto_de_programa <matheuz.lima0814@gmail.com>
2025-03-07 10:28:22 -0300

    exer1

--- exer1.php
+++ exer1.php
@@ -9,49 +9,52 @@
 </head>
 
 <body>
-    <h1>Exercicio 1</h1>
+    <div class="container mt-5">
+        <h1>Exercicio 1</h1>
+        <p>Digite os 7 valores</p>
 
-    <form method="post">
+        <form method="post">
 
-        <div class="mb-3">
-            <label for="valor1" class="form-label">Valor 1</label>
-            <input type="number" id="valor1" name="valor1" class="form-control" required="">
-        </div>
+            <div class="mb-3">
+                <label for="valor1" class="form-label">Valor 1</label>
+                <input type="number" id="valor1" name="valor1" class="form-control" required="">
+            </div>
 
-        <div class="mb-3">
-            <label for="valor2" class="form-label">Valor 2</label>
-            <input type="number" id="valor2" name="valor2" class="form-control" required="">
-        </div>
+            <div class="mb-3">
+                <label for="valor2" class="form-label">Valor 2</label>
+                <input type="number" id="valor2" name="valor2" class="form-control" required="">
+            </div>
 
-        <div class="mb-3">
-            <label for="valor3" class="form-label">Valor 3</label>
-            <input type="number" id="valor3" name="valor3" class="form-control" required="">
-        </div>
+            <div class="mb-3">
+                <label for="valor3" class="form-label">Valor 3</label>
+                <input type="number" id="valor3" name="valor3" class="form-control" required="">
+            </div>
 
-        <div class="mb-3">
-            <label for="valor4" class="form-label">Valor 4</label>
-            <input type="number" id="valor4" name="valor4" class="form-control" required="">
-        </div>
+            <div class="mb-3">
+                <label for="valor4" class="form-label">Valor 4</label>
+                <input type="number" id="valor4" name="valor4" class="form-control" required="">
+            </div>
 
-        <div class="mb-3">
-            <label for="valor5" class="form-label">Valor 5</label>
-            <input type="number" id="valor5" name="valor5" class="form-control" required="">
-        </div>
+            <div class="mb-3">
+                <label for="valor5" class="form-label">Valor 5</label>
+                <input type="number" id="valor5" name="valor5" class="form-control" required="">
+            </div>
 
-        <div class="mb-3">
-            <label for="valor6" class="form-label">Valor 6 </label>
-            <input type="number" id="valor6" name="valor6" class="form-control" required="">
-        </div>
+            <div class="mb-3">
+                <label for="valor6" class="form-label">Valor 6 </label>
+                <input type="number" id="valor6" name="valor6" class="form-control" required="">
+            </div>
 
-        <div class="mb-3">
-            <label for="valor7" class="form-label">Valor 7</label>
-            <input type="number" id="valor7" name="valor7" class="form-control" required="">
-        </div>
+            <div class="mb-3">
+                <label for="valor7" class="form-label">Valor 7</label>
+                <input type="number" id="valor7" name="valor7" class="form-control" required="">
+            </div>
 
-        <button type="submit" class="btn btn-primary">Enviar</button>
-    </form>
+            <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    </div>
 </body>
 
 </html>
