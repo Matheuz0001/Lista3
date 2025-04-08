2fd6d9bf356d73d318cc126259edf2d57c50846b
garoto_de_programa <matheuz.lima0814@gmail.com>
2025-04-08 15:40:07 -0300

    exer1

--- exer1.php
+++ exer1.php
@@ -1,60 +1,45 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['valores'])) {
+    $_SESSION['valores'] = [];
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $valor = intval($_POST['valor'] ?? 0);
+    $_SESSION['valores'][] = $valor;
+    
+    if (count($_SESSION['valores']) === 7) {
+        header("Location: respostaexer1.php");
+        exit;
+    }
+}
+?>
+
 <!doctype html>
 <html lang="pt-br">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Exercicio 1</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-
 <body>
     <div class="container mt-5">
-        <h1>Exercicio 1</h1>
-        <p>Digite os 7 valores</p>
+        <h1 class="mb-4">Exercicio 1</h1>
+        <p>Digite o valor <?php echo count($_SESSION['valores']) + 1; ?></p>
 
         <form method="post" action="respostaexer1.php">
-
-            <div class="mb-3">
-                <label for="valor1" class="form-label">Valor 1</label>
-                <input type="number" id="valor1" name="valor1" class="form-control" required="">
-            </div>
-
-            <div class="mb-3">
-                <label for="valor2" class="form-label">Valor 2</label>
-                <input type="number" id="valor2" name="valor2" class="form-control" required="">
-            </div>
-
-            <div class="mb-3">
-                <label for="valor3" class="form-label">Valor 3</label>
-                <input type="number" id="valor3" name="valor3" class="form-control" required="">
-            </div>
-
-            <div class="mb-3">
-                <label for="valor4" class="form-label">Valor 4</label>
-                <input type="number" id="valor4" name="valor4" class="form-control" required="">
-            </div>
-
-            <div class="mb-3">
-                <label for="valor5" class="form-label">Valor 5</label>
-                <input type="number" id="valor5" name="valor5" class="form-control" required="">
-            </div>
-
-            <div class="mb-3">
-                <label for="valor6" class="form-label">Valor 6 </label>
-                <input type="number" id="valor6" name="valor6" class="form-control" required="">
-            </div>
-
-            <div class="mb-3">
-                <label for="valor7" class="form-label">Valor 7</label>
-                <input type="number" id="valor7" name="valor7" class="form-control" required="">
-            </div>
-
+            <?php for ($i = 1; $i <= 7; $i++) : ?>
+                <div class="mb-3">
+                    <label for="valor<? +$i ?>" class="form-label">Valor <? +$i ?>:</label>
+                    <input type="number" id="valor<? +$i ?>" name="valor<? +$i ?>" class="form-control" required="">
+                </div>
+            <?php endfor; ?>
             <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
